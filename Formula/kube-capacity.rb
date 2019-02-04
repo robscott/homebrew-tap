26909b9199dd11cbd616c6bbb166c63438c08905
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-03 23:07:24 -0500

    Brew formula update for kube-capacity version 0.1.1

--- Formula/kube-capacity.rb
+++ Formula/kube-capacity.rb
@@ -1,9 +1,9 @@
 class KubeCapacity < Formula
   desc "Reverse Lookup for Kubernetes RBAC"
   homepage ""
-  url "https://github.com/robscott/kube-capacity/releases/download/0.1.0/kube-capacity_0.1.0_Darwin_x86_64.tar.gz"
-  version "0.1.0"
-  sha256 "63e839fb4afe1146c218a19bb03ba9619476ce97d9dffbabcf1a5069e91d3b98"
+  url "https://github.com/robscott/kube-capacity/releases/download/0.1.1/kube-capacity_0.1.1_Darwin_x86_64.tar.gz"
+  version "0.1.1"
+  sha256 "bc953a94a807d539db142093b7fed6aadf0d401d8ff9ee61001f1a154db9cd22"
 
   def install
     bin.install "kube-capacity"
