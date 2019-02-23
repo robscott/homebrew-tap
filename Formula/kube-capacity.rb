6adccad58babc2f5e15303d5c3605b869dc20a66
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-23 17:27:27 -0500

    Brew formula update for kube-capacity version 0.2.0

--- Formula/kube-capacity.rb
+++ Formula/kube-capacity.rb
@@ -1,9 +1,9 @@
 class KubeCapacity < Formula
   desc "kube-capacity provides an overview of the resource requests, limits, and utilization in a Kubernetes cluster"
   homepage ""
-  url "https://github.com/robscott/kube-capacity/releases/download/0.1.3/kube-capacity_0.1.3_Darwin_x86_64.tar.gz"
-  version "0.1.3"
-  sha256 "e11208451a9ac5fd16ba1bb911b5836b742dc20a04897fd95905f843efbb596d"
+  url "https://github.com/robscott/kube-capacity/releases/download/0.2.0/kube-capacity_0.2.0_Darwin_x86_64.tar.gz"
+  version "0.2.0"
+  sha256 "19a6c18385a8a6c9b162bb7838f4ee2198ddb41788ab69a17ada73fd4230d8ce"
 
   def install
     bin.install "kube-capacity"
